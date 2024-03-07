fc0c16fa72621ec246d429e6e3618ac8b176c069
Daniel Hernandez <barrowghoul@hotmail.com>
2024-03-07 10:00:41 -0600

    Restore appserviceprovider  settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,12 +22,12 @@ class AppServiceProvider extends ServiceProvider
     {
 
         $keys = ['pusher_app_id', 'pusher_app_key', 'pusher_app_secret', 'pusher_app_cluster'];
-        //$pusherConf = Setting::whereIn('key', $keys)->pluck('value','key');
+        $pusherConf = Setting::whereIn('key', $keys)->pluck('value','key');
 
-        //config(['broadcasting.connections.pusher.key' => $pusherConf['pusher_app_key']]);
-        //config(['broadcasting.connections.pusher.secret' => $pusherConf['pusher_app_secret']]);
-        //config(['broadcasting.connections.pusher.app_id' => $pusherConf['pusher_app_id']]);
-        //config(['broadcasting.connections.pusher.options.cluster' => $pusherConf['pusher_app_cluster']]);
+        config(['broadcasting.connections.pusher.key' => $pusherConf['pusher_app_key']]);
+        config(['broadcasting.connections.pusher.secret' => $pusherConf['pusher_app_secret']]);
+        config(['broadcasting.connections.pusher.app_id' => $pusherConf['pusher_app_id']]);
+        config(['broadcasting.connections.pusher.options.cluster' => $pusherConf['pusher_app_cluster']]);
 
     }
 }
